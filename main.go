7572aea964d625394ebe79ff25b82d3f0c2a0c4e
Brian Danowski <briandanowski@gmail.com>
2017-12-02 14:23:37 -0500

    init

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var content []string
+
+func init() {
+	content = append(content, `Welcome to the chalkboard
+
+edit this text
+
+right now the data is ephemeral, just like a real chalkboard ;p
+`)
+}
+
+func main() {
+	fmt.Println("Hello world!")
+
+	http.HandleFunc("/api/board", apiHandler)
+
+	static := http.FileServer(http.Dir("static"))
+	http.Handle("/", static)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		content = append(content, string(body))
+		w.WriteHeader(http.StatusOK)
+		return
+
+	case http.MethodGet:
+		fmt.Fprintf(w, content[len(content)-1])
+
+	}
+}
